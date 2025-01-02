590398c2bff625e44b40260ee083dff9979549c7
snsumit <ss2153436@gmail.com>
2025-01-02 17:02:00 +0530

    added files

--- frontend/src/pages/CaptainSignup.jsx
+++ frontend/src/pages/CaptainSignup.jsx
@@ -55,6 +55,10 @@ const CaptainSignup = () => {
       setLastName('')
       setEmail('')
       setPassword('')
+      setVehicleColor('')
+      setVehiclePlate('')
+      setVehicleCapacity('')
+      setVehicleType('')
    }
 
    return (
